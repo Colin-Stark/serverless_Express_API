34d33bd3fe151ea19a2e5dcbb2885fa6832c2a24
Colin-Stark <collinscodes@gmail.com>
2025-12-26 12:59:45 -0500

    Feat: Update root route response to return JSON message

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-
+// Define your routes
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.json({ message: 'Hello from Express on Vercel!' });
 });
 
+// Export the Express app
 module.exports = app;
